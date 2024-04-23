f881b71c208c1124724c81f476c550076b7099ab
Prashant <jatinverma200025@gmail.com>
2024-04-23 22:01:32 +0530

    fixed bg image

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import Home from "./pages/Home";
+import bgImg from "./assets/bg.jpg";
 
 function App() {
   return (
-    <div className="bg-[url('./src/assets/bg.jpg')] bg-cover bg-center text-slate-200 min-h-[100dvh]">
+    <div className="text-slate-200 min-h-[100dvh]">
+      <img
+        src={bgImg}
+        alt="background"
+        className="absolute top-0 left-0 w-full h-full object-cover -z-10"
+      />
       <Home />
     </div>
   );
